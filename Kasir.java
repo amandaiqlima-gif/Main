8a4a98410751d7e6c93dea6c836259ad7f74a3e7
amandaiqlima-gif <amanda.iqlima@student.undiksha.ac.id>
2026-05-06 14:50:05 +0800

    Add files via upload

---
+++ Kasir.java
@@ -0,0 +1,11 @@
+class Kasir {
+    String namaKasir;
+
+    Kasir(String namaKasir) {
+        this.namaKasir = namaKasir;
+    }
+
+    void tampilKasir() {
+        System.out.println("Kasir: " + namaKasir);
+    }
+}
